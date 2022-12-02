0c333fb02cf054ff3286fc3b3a3a13e6dbdd26d0
Pradyumna Chatterjee <chatterjee.pra@gmail.com>
2022-12-02 19:34:05 +0530

    added file ext and created code list go file

--- fetch/getBseCodes.go
+++ fetch/getBseCodes.go
@@ -2,30 +2,30 @@ package main
 
 import (
 	"encoding/json"
-	"os"
-	"strings"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 	"time"
 )
 
 type scrip struct {
-	SCRIP_CD    string 
-    Scrip_Name  string 
-    Status      string 
-    GROUP       string 
-    FACE_VALUE  string 
-    ISIN_NUMBER string 
-    INDUSTRY    string 
-    Scrip_id    string 
-    Segment 	string 
-    NSURL 		string 
-    Issuer_Name string 
-    Mktcap 		string 
+	SCRIP_CD    string
+	Scrip_Name  string
+	Status      string
+	GROUP       string
+	FACE_VALUE  string
+	ISIN_NUMBER string
+	INDUSTRY    string
+	Scrip_id    string
+	Segment     string
+	NSURL       string
+	Issuer_Name string
+	Mktcap      string
 }
 
-func structToCSV( scrips []scrip) {
+func structToCSV(scrips []scrip) {
 	f, err := os.Create("res/bse/list.csv")
 	if err != nil {
 		log.Fatal(err)
@@ -35,24 +35,21 @@ func structToCSV( scrips []scrip) {
 
 	// _, err = f.WriteString("SCRIP_CD, Scrip_Name, Status, GROUP, FACE_VALUE, ISIN_NUMBER, INDUSTRY, Scrip_id, Segment, NSURL, Issuer_Name,  Mktcap\r\n")
 	_, err = f.WriteString("Scrip_id, SCRIP_CD, ISIN_NUMBER, Scrip_Name, NSURL, INDUSTRY, GROUP, FACE_VALUE,  Issuer_Name,  Mktcap\r\n")
-    if err != nil {
+	if err != nil {
 		log.Fatal(err)
 	}
 
-	for _ , sym := range scrips {
-		f.WriteString(sym.Scrip_id+", "+sym.SCRIP_CD+", "+sym.ISIN_NUMBER+", "+sym.Scrip_Name+", "+sym.NSURL+", "+sym.INDUSTRY+", "+sym.GROUP+", "+sym.FACE_VALUE+", "+strings.Replace(sym.Issuer_Name,",","",-1)+", "+sym.Mktcap+"\r\n");
+	for _, sym := range scrips {
+		f.WriteString(sym.Scrip_id + ", " + sym.SCRIP_CD + ", " + sym.ISIN_NUMBER + ", " + sym.Scrip_Name + ", " + sym.NSURL + ", " + sym.INDUSTRY + ", " + sym.GROUP + ", " + sym.FACE_VALUE + ", " + strings.Replace(sym.Issuer_Name, ",", "", -1) + ", " + sym.Mktcap + "\r\n")
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
 
 	f.Sync()
-
 }
 
-
 func fetchBSE() {
-
 	url := "https://api.bseindia.com/BseIndiaAPI/api/ListofScripData/w?Group=&Scripcode=&industry=&segment=Equity&status=Active"
 
 	spaceClient := http.Client{
@@ -89,9 +86,7 @@ func fetchBSE() {
 		log.Fatal(jsonErr)
 	}
 
-
 	structToCSV(scrips)
-	
 }
 
 func fetchNSE() {
@@ -122,8 +117,6 @@ func fetchNSE() {
 		log.Fatal(readErr)
 	}
 
-
-
 	f, err := os.Create("res/nse/list.csv")
 	if err != nil {
 		log.Fatal(err)
@@ -133,7 +126,6 @@ func fetchNSE() {
 
 	_, err = f.Write(body)
 	f.Sync()
-    
 }
 
 func main() {
