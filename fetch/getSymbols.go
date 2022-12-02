a7405d364303acdae51e0a03eb33ba910b02e621
Pradyumna Chatterjee <chatterjee.pra@gmail.com>
2022-12-02 19:53:38 +0530

    added fetchers for symbols

--- fetch/getSymbols.go
+++ fetch/getSymbols.go
@@ -1,4 +1,4 @@
-package main
+package symbols
 
 import (
 	"encoding/json"
@@ -6,50 +6,10 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"strings"
 	"time"
 )
 
-type scrip struct {
-	SCRIP_CD    string
-	Scrip_Name  string
-	Status      string
-	GROUP       string
-	FACE_VALUE  string
-	ISIN_NUMBER string
-	INDUSTRY    string
-	Scrip_id    string
-	Segment     string
-	NSURL       string
-	Issuer_Name string
-	Mktcap      string
-}
-
-func structToCSV(scrips []scrip) {
-	f, err := os.Create("res/bse/list.csv")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer f.Close()
-
-	// _, err = f.WriteString("SCRIP_CD, Scrip_Name, Status, GROUP, FACE_VALUE, ISIN_NUMBER, INDUSTRY, Scrip_id, Segment, NSURL, Issuer_Name,  Mktcap\r\n")
-	_, err = f.WriteString("Scrip_id, SCRIP_CD, ISIN_NUMBER, Scrip_Name, NSURL, INDUSTRY, GROUP, FACE_VALUE,  Issuer_Name,  Mktcap\r\n")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, sym := range scrips {
-		f.WriteString(sym.Scrip_id + ", " + sym.SCRIP_CD + ", " + sym.ISIN_NUMBER + ", " + sym.Scrip_Name + ", " + sym.NSURL + ", " + sym.INDUSTRY + ", " + sym.GROUP + ", " + sym.FACE_VALUE + ", " + strings.Replace(sym.Issuer_Name, ",", "", -1) + ", " + sym.Mktcap + "\r\n")
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	f.Sync()
-}
-
-func fetchBSE() {
+func fetchBSE(csvFilePath string) {
 	url := "https://api.bseindia.com/BseIndiaAPI/api/ListofScripData/w?Group=&Scripcode=&industry=&segment=Equity&status=Active"
 
 	spaceClient := http.Client{
@@ -86,10 +46,10 @@ func fetchBSE() {
 		log.Fatal(jsonErr)
 	}
 
-	structToCSV(scrips)
+	structToCSV(scrips, csvFilePath)
 }
 
-func fetchNSE() {
+func fetchNSE(csvFilePath string) {
 	url := "https://www1.nseindia.com/content/equities/EQUITY_L.csv"
 
 	spaceClient := http.Client{
@@ -117,7 +77,7 @@ func fetchNSE() {
 		log.Fatal(readErr)
 	}
 
-	f, err := os.Create("res/nse/list.csv")
+	f, err := os.Create(csvFilePath)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -129,6 +89,6 @@ func fetchNSE() {
 }
 
 func main() {
-	// fetchBSE()
-	fetchNSE()
+	fetchBSE("../data/symbols/bse.csv")
+	fetchNSE("../data/symbols/nse.csv")
 }
---
+++ fetch/utils.go
@@ -0,0 +1,46 @@
+package symbols
+
+import (
+	"log"
+	"os"
+	"strings"
+)
+
+type scrip struct {
+	SCRIP_CD    string
+	Scrip_Name  string
+	Status      string
+	GROUP       string
+	FACE_VALUE  string
+	ISIN_NUMBER string
+	INDUSTRY    string
+	Scrip_id    string
+	Segment     string
+	NSURL       string
+	Issuer_Name string
+	Mktcap      string
+}
+
+func structToCSV(scrips []scrip, csvFilePath string) {
+	f, err := os.Create(csvFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	// _, err = f.WriteString("SCRIP_CD, Scrip_Name, Status, GROUP, FACE_VALUE, ISIN_NUMBER, INDUSTRY, Scrip_id, Segment, NSURL, Issuer_Name,  Mktcap\r\n")
+	_, err = f.WriteString("Scrip_id, SCRIP_CD, ISIN_NUMBER, Scrip_Name, NSURL, INDUSTRY, GROUP, FACE_VALUE,  Issuer_Name,  Mktcap\r\n")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, sym := range scrips {
+		f.WriteString(sym.Scrip_id + ", " + sym.SCRIP_CD + ", " + sym.ISIN_NUMBER + ", " + sym.Scrip_Name + ", " + sym.NSURL + ", " + sym.INDUSTRY + ", " + sym.GROUP + ", " + sym.FACE_VALUE + ", " + strings.Replace(sym.Issuer_Name, ",", "", -1) + ", " + sym.Mktcap + "\r\n")
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	f.Sync()
+}
